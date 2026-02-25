1b0ff01cf55de4f7bdf12407bb2bd5d876669318
Your Name <you@example.com>
2026-02-25 17:50:36 +0200

    some massage

--- script.js
+++ script.js
@@ -118,15 +118,15 @@ var WORKS = [
 ];
 
 var SLIDES = [
-  'https://drive.google.com/file/d/1_ffF1_XPj_VnVls2roC5_vEaGSGxaqg9/view?usp=FILE_ID',  // Slide 3
-  'https://drive.google.com/file/d/1QK7M9YSFqx5rJJosJkWPza66E87yFxjn/view?usp=FILE_ID',  // Slide 1
-  'https://drive.google.com/file/d/1nkN2oN1tuCJQ1PpX3WRqWqNi_jrYzzPL/view?usp=FILE_ID',  // Slide 2
-  'https://drive.google.com/file/d/1nkN2oN1tuCJQ1PpX3WRqWqNi_jrYzzPL/view?usp=FILE_ID',  // Slide 4
-'https://drive.google.com/file/d/1ffEK_x7fBltP0fz6DtkrwrCHtR6hrgx5/view?usp=FILE_ID',  // Slide 5
-'https://drive.google.com/file/d/1gj86jXayWZb2EkFnk1qRh1L6KU-ovVlN/view?usp=FILE_ID',  // Slide 6
-'https://drive.google.com/file/d/1p7M5P0sNb2Cs5BTiaF4qOCEXpaj6sjhy/view?usp=FILE_ID',  // Slide 7
-'https://drive.google.com/file/d/1gj86jXayWZb2EkFnk1qRh1L6KU-ovVlN/view?usp=FILE_ID',  // Slide 8
-'https://drive.google.com/file/d/1nkN2oN1tuCJQ1PpX3WRqWqNi_jrYzzPL/view?usp=FILE_ID',  // Slide 9
+  'https://drive.google.com/file/d/1_ffF1_XPj_VnVls2roC5_vEaGSGxaqg9/view?usp=sharing',  // Slide 3
+  'https://drive.google.com/file/d/1QK7M9YSFqx5rJJosJkWPza66E87yFxjn/view?usp=sharing',  // Slide 1
+  'https://drive.google.com/file/d/1-R7kV9K1sJ0R7VWckvnTgZazPnq38RnY/view?usp=sharing',  // Slide 2
+  'https://drive.google.com/file/d/1ffEK_x7fBltP0fz6DtkrwrCHtR6hrgx5/view?usp=sharing',  // Slide 4
+'https://drive.google.com/file/d/1KuF-V4j-5B3kWuEMC6UU8Ksb37A77Jn_/view?usp=sharing',  // Slide 5
+'https://drive.google.com/file/d/1gj86jXayWZb2EkFnk1qRh1L6KU-ovVlN/view?usp=sharing',  // Slide 6
+'https://drive.google.com/file/d/1p7M5P0sNb2Cs5BTiaF4qOCEXpaj6sjhy/view?usp=sharing',  // Slide 7
+'https://drive.google.com/file/d/1mx3ekKi85R2MNPgSlnhy47i9tPhs7guH/view?usp=sharing',  // Slide 8
+'https://drive.google.com/file/d/1_fhS4Bui57pPH65J2wzLQWyeYQjGPL6R/view?usp=sharing',  // Slide 9
 
 ];
 
